180c5d93e3acfed9cf7b465e5fc44b51644e1d42
jllloydd <wayfivet@gmail.com>
2025-04-14 11:01:52 +0800

    Fixed UI of login pages to make it flow better

--- resources/views/tasksview.blade.php
+++ resources/views/tasksview.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-6 sm:py-12">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
             <!-- Create Task Button -->
             <div class="mb-4 flex justify-end">
                 <button onclick="openModal()"
